e4ae55ea78f04cb9a1b3e4c4a2652fd7963b750a
sirdabalot <sirdabalot@hotmail.co.uk>
2015-07-15 18:33:17 +0100

    Added WPF interpreter

---
+++ OCLogInterpreter/OCLogInterpreter/LogFile.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace OCLogInterpreter
+{
+    class LogFile
+    {
+        public List<Entry> m_entryList = new List<Entry>();
+
+        public List<string> m_listOfPriorities = new List<string>();
+
+        public void populatePriorities()
+        {
+            m_listOfPriorities.Clear();
+
+            m_listOfPriorities.Add("");
+
+            foreach (Entry e in m_entryList)
+            {
+                if (!m_listOfPriorities.Contains(e.m_priority))
+                {
+                    m_listOfPriorities.Add(e.m_priority);
+                }
+            }
+        }
+
+        public List<Entry> findEntriesByTimeDate(string filterTimeDate)
+        {
+            List<Entry> retList = new List<Entry>();
+
+            foreach (Entry e in m_entryList)
+            {
+                if (e.m_dateTime.Contains(filterTimeDate))
+                {
+                    retList.Add(e);
+                }
+            }
+
+            return retList;
+        }
+
+        public List<Entry> findEntriesByPriority(string filterPriority)
+        {
+            List<Entry> retList = new List<Entry>();
+
+            foreach (Entry e in m_entryList)
+            {
+                if (e.m_priority.Contains(filterPriority))
+                {
+                    retList.Add(e);
+                }
+            }
+
+            return retList;
+        }
+
+        public List<Entry> filterEntries(string filterPriority, string filterDateTime)
+        {
+            List<Entry> retList = new List<Entry>();
+
+            foreach (Entry e in m_entryList)
+            {
+                if (e.m_priority.Contains(filterPriority) && e.m_dateTime.Contains(filterDateTime) )
+                {
+                    retList.Add(e);
+                }
+            }
+
+            return retList;
+        }
+
+        public void loadFromFile(string path)
+        {
+            m_entryList.Clear();
+            m_listOfPriorities.Clear();
+
+            StreamReader sr = new StreamReader(path);
+
+            try
+            {
+                while (!sr.EndOfStream)
+                {
+                    m_entryList.Add(Entry.unSerialize(sr.ReadLine()));
+                }
+                populatePriorities();
+            }
+            finally
+            {
+                sr.Close();
+            }
+        }
+    }
+
+    class Entry
+    {
+        public string m_dateTime = "";
+        public string m_info = "";
+        public string m_priority = "";
+
+        public override string ToString()
+        {
+            return m_dateTime + ": " + m_priority;
+        }
+
+        public static Entry unSerialize(string data)
+        {
+
+            Entry newEntry = new Entry();
+
+            int part = 0;
+
+            char[] charrdata = data.ToCharArray();
+
+            foreach (char c in charrdata)
+            {
+                if (c == '|')
+                {
+                    part++;
+                }
+                else
+                {
+                    switch (part)
+                    {
+                        case 1:
+                            newEntry.m_dateTime += c;
+                            break;
+                        case 2:
+                            newEntry.m_info += c;
+                            break;
+                        case 3:
+                            newEntry.m_priority += c;
+                            break;
+                    }
+                }
+            }
+
+            return newEntry;
+        }
+    }
+}
---
+++ OCLogInterpreter/OCLogInterpreter/MainWindow.xaml.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace OCLogInterpreter
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        LogFile lf;
+        Microsoft.Win32.OpenFileDialog ofd;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            lf = new LogFile();
+            ofd = new Microsoft.Win32.OpenFileDialog();
+
+            EntriesView.ItemsSource = lf.m_entryList;
+            PriorityFilterDD.ItemsSource = lf.m_listOfPriorities;
+        }
+
+        private void OpenLogFile(object sender, RoutedEventArgs e)
+        {
+            ofd.DefaultExt = ".log";
+            ofd.Filter = "OpenComputers Log Files (.log)|*.log";
+            bool? result = ofd.ShowDialog();
+
+            if ( result == true )
+            {
+                lf.loadFromFile(ofd.FileName);
+            }
+            else
+            {
+                MessageBox.Show("Error opening file!");
+            }
+
+            refreshAllItems();
+        }
+
+        private void refreshAllItems()
+        {
+            EntriesView.Items.Refresh();
+            PriorityFilterDD.Items.Refresh();
+        }
+        
+        private void EntriesView_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Entry selectedEntry = (Entry) EntriesView.SelectedItem;
+
+            if (selectedEntry != null)
+            {
+                SelTimeDateLab.Content = "Date/Time: " + selectedEntry.m_dateTime;
+                SelPriorityLab.Content = "Priority: " + selectedEntry.m_priority;
+                SelInfoTB.Text = selectedEntry.m_info;
+            }
+        }
+
+        private void RenewFilter(object sender, RoutedEventArgs e)
+        {
+            if (FilterCB != null)
+            {
+                if (FilterCB.IsChecked == true)
+                {
+                    EntriesView.ItemsSource = lf.filterEntries(PriorityFilterDD.Text, DateFilterTB.Text);
+                }
+                else
+                {
+                    EntriesView.ItemsSource = lf.m_entryList;
+                }
+
+                EntriesView.Items.Refresh();
+            }
+        }
+
+    }
+}
